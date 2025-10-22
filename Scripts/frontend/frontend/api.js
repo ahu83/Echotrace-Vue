f9dc805b693d9a6018f687a8294b07dfd0871051
ahu83 <ahuxam21@gmail.com>
2025-10-23 03:24:58 +1100

    Update api.js

--- Scripts/frontend/frontend/api.js
+++ Scripts/frontend/frontend/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:5000",
+  baseURL: "https://setting-milwaukee-politicians-core.trycloudflare.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
