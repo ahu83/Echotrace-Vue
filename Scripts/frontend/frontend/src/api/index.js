8ad88dcf98118afb0c2a57eb93be238ea93648d6
ahu83 <ahuxam21@gmail.com>
2025-10-24 14:51:12 +1100

    update api

--- Scripts/frontend/frontend/src/api/index.js
+++ Scripts/frontend/frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL || "http://127.0.0.1:5000",
+  baseURL: "https://trips-enables-sellers-premium.trycloudflare.com"  || "http://127.0.0.1:5000",
   headers: {
     "Content-Type": "application/json",
   },
